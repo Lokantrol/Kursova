d408df561b48a240ba7e1fdcb1a2fc49ae8e85c8
BasiukM <101507825+Lokantrol@users.noreply.github.com>
2023-06-14 17:08:13 +0300

    Сервер+клієнт

---
+++ Connect_to_serv/Connect_to_serv/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TcpClient client = new TcpClient();
+        client.Connect("127.0.0.1", 8080);
+
+        NetworkStream stream = client.GetStream();
+
+        // Відправка повідомлення CONNECT
+        byte[] connectPacket = { 0x01 };
+        stream.Write(connectPacket, 0, connectPacket.Length);
+
+        // Очікування повідомлення CONNACK
+        byte[] response = new byte[1];
+        int bytesRead = stream.Read(response, 0, response.Length);
+
+        if (bytesRead > 0 && response[0] == 0x02)
+        {
+            Console.WriteLine("Connection established.");
+
+            // Відправка повідомлення DISCON
+            byte[] disconnectPacket = { 0x03 };
+            stream.Write(disconnectPacket, 0, disconnectPacket.Length);
+
+            // Очікування повідомлення DISCONACK
+            bytesRead = stream.Read(response, 0, response.Length);
+
+            if (bytesRead > 0 && response[0] == 0x04)
+            {
+                Console.WriteLine("Disconnected from the server.");
+            }
+        }
+        Console.ReadLine();
+        stream.Close();
+        client.Close();
+        Console.ReadLine();
+    }
+}
---
+++ serv/ConsoleApp11/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 8080);
+        server.Start();
+
+        Console.WriteLine("Waiting for incoming connections...");
+
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Client connected.");
+
+        NetworkStream stream = client.GetStream();
+
+        // Очікування повідомлення CONNECT
+        byte[] connectPacket = new byte[1];
+        int bytesRead = stream.Read(connectPacket, 0, connectPacket.Length);
+
+        if (bytesRead > 0 && connectPacket[0] == 0x01)
+        {
+            Console.WriteLine("Connection established.");
+
+            // Відправка повідомлення CONNACK
+            byte[] connackPacket = { 0x02 };
+            stream.Write(connackPacket, 0, connackPacket.Length);
+            Console.ReadLine();
+            // Очікування повідомлення DISCON
+            byte[] disconnectPacket = new byte[1];
+            bytesRead = stream.Read(disconnectPacket, 0, disconnectPacket.Length);
+
+            if (bytesRead > 0 && disconnectPacket[0] == 0x03)
+            {
+                Console.WriteLine("Received disconnect request.");
+
+                // Відправка повідомлення DISCONACK
+                byte[] disconackPacket = { 0x04 };
+                stream.Write(disconackPacket, 0, disconackPacket.Length);
+
+                Console.WriteLine("Disconnected from the client.");
+            }
+        }
+        Console.ReadKey();
+        stream.Close();
+        client.Close();
+        if (Console.ReadLine() == "close") ;
+        {
+            server.Stop();
+        }
+        Console.ReadLine();
+    }
+}
